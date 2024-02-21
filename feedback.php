41964d0c9b182b4e0451f9c327a24a8a8384bfb8
UttamDholariya <uttamdholariya1@gmail.com>
2024-02-21 20:35:14 +0530

    ugg

--- feedback.php
+++ feedback.php
@@ -1,13 +1,14 @@
 <?php
         if(isset($_POST['feed']))
         {
-            $conn = mysqli_connect('localhost', 'root', '', 'drtools') or die("Connection Faild") . mysqli_connect_error();
+            include "confing.php";
+            
             $full_name = mysqli_real_escape_string($conn,$_POST['full_name']); 
             $email = mysqli_real_escape_string($conn,$_POST['email']); 
             $phone = mysqli_real_escape_string($conn,md5($_POST['phone'])); 
             $message = mysqli_real_escape_string($conn,md5($_POST['message']));
             
-            echo $sql = "INSERT INTO contactus (full_name,email, phone,message) VALUES ('{$full_name}','{$email}','{$phone}','{$message})";
+            $sql = "INSERT INTO contactus (full_name,email, phone,message) VALUES ('{$full_name}','{$email}','{$phone}','{$message})";
             if(mysqli_query($conn,$sql))
             {
                 header('Location: contact-us.php');   
