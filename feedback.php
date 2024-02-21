2da5e2a5a80605dab9e83aa57cb184b76554395b
UttamDholariya <uttamdholariya1@gmail.com>
2024-02-21 20:36:21 +0530

    wewewe

--- feedback.php
+++ feedback.php
@@ -2,7 +2,7 @@
         if(isset($_POST['feed']))
         {
             include "confing.php";
-            
+
             $full_name = mysqli_real_escape_string($conn,$_POST['full_name']); 
             $email = mysqli_real_escape_string($conn,$_POST['email']); 
             $phone = mysqli_real_escape_string($conn,md5($_POST['phone'])); 
@@ -11,7 +11,7 @@
             $sql = "INSERT INTO contactus (full_name,email, phone,message) VALUES ('{$full_name}','{$email}','{$phone}','{$message})";
             if(mysqli_query($conn,$sql))
             {
-                header('Location: contact-us.php');   
+                header("Location: contact-us.php");   
             }
             else{
                 echo "Query Failed";
