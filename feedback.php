f4e70ea17ff5b4d87b5c8353217f9a9db6353834
UttamDholariya <uttamdholariya1@gmail.com>
2024-02-21 20:39:36 +0530

    hshshss

--- feedback.php
+++ feedback.php
@@ -5,8 +5,8 @@
 
             $full_name = mysqli_real_escape_string($conn,$_POST['full_name']); 
             $email = mysqli_real_escape_string($conn,$_POST['email']); 
-            $phone = mysqli_real_escape_string($conn,md5($_POST['phone'])); 
-            $message = mysqli_real_escape_string($conn,md5($_POST['message']));
+            $phone = mysqli_real_escape_string($conn,$_POST['phone']); 
+            $message = mysqli_real_escape_string($conn,$_POST['message']);
             
             $sql = "INSERT INTO contactus (full_name,email, phone,message) VALUES ('{$full_name}','{$email}','{$phone}','{$message})";
             if(mysqli_query($conn,$sql))
