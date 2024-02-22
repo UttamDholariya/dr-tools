11e5d4cbb805559d8dfc1f92cc95c9dfff500b56
nainsirathod <rathodnainsi@gmail.com>
2024-02-22 22:49:19 +0530

    init

--- admin/view_message.php
+++ admin/view_message.php
@@ -10,24 +10,33 @@
             </ol>
         </nav>
     </div>
+    
     <section class="section">
         <div class="row">
             <div class="col-lg-12">
                 <div class="card shadow">
                     <div class="card-body">
                         <h5 class="card-title">View Message</h5>
+                        <?php 
+                            include "confing.php";
+                            $id = $_GET['message_id'];  
+                            $sql = "SELECT * FROM contactus WHERE id = {$id}";
+                            $result = mysqli_query($conn,$sql) or die("Query Feiled");
+                            if(mysqli_num_rows($result) > 0){
+                                while($row = mysqli_fetch_assoc($result)){
+                        ?>
                         <form class="row g-3">
                             
                             <div class="col-md-12">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="message_id" value="fatched message id" disabled>
+                                    <input type="text" class="form-control" id="message_id" value="<?php echo $row['message_id']; ?>" disabled>
                                     <label for="category_name">Message Id :</label>
                                 </div>
                             </div>
 
                              <div class="col-md-12">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="user_name" value="fatched user name" disabled>
+                                    <input type="text" class="form-control" id="full_name" value="<?php echo $row['full_name']; ?>" disabled>
                                     <label for="category_name">User Name :</label>
                                 </div>
                             </div>
@@ -37,21 +46,21 @@
 
                             <div class="col-md-12">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="email" value="fatched email" disabled>
+                                    <input type="text" class="form-control" id="email" value="<?php echo $row['email']; ?>" disabled>
                                     <label for="category_name">Email :</label>
                                 </div>
                             </div>
 
                             <div class="col-md-12">
                                 <div class="form-floating">
-                                    <input type="text" class="form-control" id="phone_no" value="fatched phone_no" disabled>
+                                    <input type="text" class="form-control" id="phone" value="<?php echo $row['phone']; ?>" disabled>
                                     <label for="category_name">Phone_No :</label>
                                 </div>
                             </div>
 
                             <div class="col-md-12">
                                 <div class="form-floating">
-                                    <textarea class="form-control" id="massege" value="fatched massege" disabled ></textarea>
+                                    <textarea class="form-control" id="massege" value="<?php echo $row['message']; ?>" disabled ></textarea>
                                     <label for="category_name">Massege :</label>
                                 </div>
                             </div>
@@ -60,6 +69,10 @@
                                 <button type="submit" class="btn" style="background-color: #2aa1a8;">Go Back</button>
                             </div>
                         </form>
+                        <?php
+                                }
+                            }
+                        ?>
                     </div>
                 </div>
             </div>
