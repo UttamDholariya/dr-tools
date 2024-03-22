1aaed5d770c4224da8b0ee05fe9210a3ee4fa1ba
KachhadiyaPrit <kachhadiyaprit89039@gmail.com>
2024-03-22 17:38:57 +0530

    new

--- admin/generate-report.php
+++ admin/generate-report.php
@@ -13,7 +13,8 @@
     $dom_pdf->loadHtml($html);
     $dom_pdf->setPaper('A4', 'lnandscape');
     $dom_pdf->render();
-    $dom_pdf->stream();
+    $current_date = date("Y-m-d");
+    $dom_pdf->stream("Product-Report-$current_date.pdf");
     // $pdf = $dom_pdf->output();
     // file_put_contents("ProductReport.pdf", $pdf);
 ?> 
